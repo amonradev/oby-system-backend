286e9605d508cba4e3902577d9e6814260c6258a
amonradev <amonracardoso@gmail.com>
2022-05-16 20:01:11 -0300

    test: Add initial test

---
+++ src/test/java/com/backend/oby/ObyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.oby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
