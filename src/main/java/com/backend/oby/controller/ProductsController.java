f828d2540aff4e7d6c818d8d5a04e6f98ff55d1b
amonradev <amonracardoso@gmail.com>
2022-06-26 13:59:21 -0300

    fix: Fix cors and crossorigin bug

--- src/main/java/com/backend/oby/controller/ProductsController.java
+++ src/main/java/com/backend/oby/controller/ProductsController.java
@@ -10,6 +10,7 @@ import com.backend.oby.services.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/products")
 public class ProductsController {
@@ -42,7 +44,7 @@ public class ProductsController {
 		this.productsService.createProduct(products);
 		return new ResponseEntity<>(HttpStatus.OK);
     }
-    
+
     @PutMapping("/{id}")
 	public ResponseEntity<Void> updateProducts(@RequestBody Products products) {
 		this.productsService.updateProducts(products);
